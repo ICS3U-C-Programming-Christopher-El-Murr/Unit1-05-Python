a1cb61587a269fc305dc12795ecec9ec3c1ff73f
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-09-18 18:29:07 +0000

    area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Christopher El-Murr
+# Date: 09 18, 2025
+# This program is a basic math calculator capable of calculating the area and perimeter of a rectangle
+
+
+def main():
+    print("For a rectangle with length 7cm and width 8 cm")
+    print()
+    print("The area is:{}cm^2".format(7 * 8))
+    print("The perimeter is:{}cm".format(2 * (7 + 8)))
+
+
+if __name__ == "__main__":
+    main()
